58d165aaefc3f0f50717713f737dc80de4f81bf4
Finch <sajjalf23@gmail.com>
2026-01-14 14:38:21 +0500

    deploy ready

--- client/src/Components/Relatedproducts.jsx
+++ client/src/Components/Relatedproducts.jsx
@@ -1,44 +1,8 @@
-// import React, { useContext, useEffect, useState } from 'react'
-// import { ShopContext } from '../Context/ShopContext'
-// import ProductItem from '../Components/Productitem';
-
-// const Relatedproducts = ({category , subCategory}) => {
-//     const {products} = useContext(ShopContext)
-
-//     const [related, setrelated] = useState([]);
-
-//     useEffect(()=>{
-//         if(products.length > 0){
-//             let productsCopy = products.slice();
-//             productsCopy = productsCopy.filter((item)=> item.category === category);
-//             productsCopy = productsCopy.filter((item)=> item.subCategory === subCategory);
-//             setrelated(productsCopy.slice(0,5));
-//         }
-//     },[products]);
-//   return (
-//     <div className='my-24'>
-//         <div className="text-center text-3xl py-2 ">
-//             <Title text1={"RELATED"} text2={"PRODUCTS"}></Title>
-//         </div>
-//         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-//             {
-//                 related.map((item,index)=>(
-//                     <ProductItem id={item._id} key={index} name={item.name} 
-//                     price={item.price} image={item.image}></ProductItem>
-//                 ))
-//             }
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default Relatedproducts
-
 
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../Context/ShopContext";
-import ProductItem from "../Components/ProductItem";
-import Title from "../Components/Title"; 
+import ProductItem from "../Components/Productitem";
+import Title from "/Title"; 
 
 const RelatedProducts = ({ category, subCategory }) => {
   const { products } = useContext(ShopContext);
